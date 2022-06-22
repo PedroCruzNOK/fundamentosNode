ae636325386ccf51a422289364ab1ce6c0700d45
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2022-06-22 11:12:45 -0500

    monohilo

---
+++ conceptos/monohilo.js
@@ -0,0 +1,14 @@
+console.log("hola mis amigos");
+
+let i = 0;
+setInterval(function(){
+    console.log(i);
+    i++;
+
+    if (i === 4){
+        var a = 3 + z;
+    }
+
+}, 1000);
+
+console.log("segundo mensaje");
